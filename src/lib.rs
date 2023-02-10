399dd86a34e3b3ec2c741948107e2e6a7beed056
remtori <lqvu99@gmail.com>
2023-02-10 16:29:23 +0700

    vrf-lib: impl bound() helper + tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
+use std::ops::RangeInclusive;
+
 use anchor_lang::{prelude::*, InstructionData};
+use num_traits::{AsPrimitive, PrimInt};
 
 #[event]
 pub struct RequestVrf {
@@ -55,4 +58,51 @@ impl Default for VrfResult {
 impl VrfResult {
     pub const RANDOM_BYTE_LEN: usize = 16;
     pub const SIGNATURE_BYTE_LEN: usize = 64;
+
+    #[allow(unused)]
+    fn new(random: [u8; VrfResult::RANDOM_BYTE_LEN]) -> Self {
+        Self {
+            random,
+            request_transaction: [0u8; VrfResult::SIGNATURE_BYTE_LEN],
+        }
+    }
+
+    pub fn bound<T>(self, range: RangeInclusive<T>) -> T
+    where
+        T: PrimInt + AsPrimitive<i128>,
+        i128: AsPrimitive<T>,
+    {
+        let v = i128::from_be_bytes(self.random);
+        let bound: i128 = (*range.end() - *range.start()).as_();
+        ((v % bound) + range.start().as_()).as_()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[derive(AnchorSerialize)]
+    struct A {
+        random: VrfResult,
+        value: u32,
+    }
+
+    #[test]
+    fn test() {
+        let a = A {
+            random: VrfResult::default(),
+            value: 1234,
+        };
+
+        let mut v = Vec::new();
+        a.serialize(&mut v).unwrap();
+        println!("{:?}\n{:?}", &v[0..32], &v[32..]);
+    }
+
+    #[test]
+    fn r1() {
+        let r = VrfResult::new([111, 118, 107, 173, 240, 168, 69, 73, 10, 9, 142, 105, 124, 62, 45, 22]);
+        println!("{}", r.bound(0u64..=u32::MAX as u64));
+    }
 }
